c04841c1f7377c0b0997ff5b1bc6a0381ad4552c
feudy <vaz.justin@gmail.com>
2015-09-23 14:15:24 +0300

    some code fixes, comments

--- hello.js
+++ hello.js
@@ -1,16 +1,8 @@
 Router.route('/', {
-
-    action: function () {
-        if (this.ready())
-            return true;
-    }
 });
 
 Markers = new Meteor.Collection('markers');
-
-
 if (Meteor.isClient) {
-
     Template.locations.events({
         'click .directions': function (evt, tmpl) {
             var item = evt.target;
@@ -35,23 +27,14 @@ if (Meteor.isClient) {
         }
     });
 
-
-
-
     var map;
     Template.mapview.rendered = function () {
-
         var directionsService = new google.maps.DirectionsService;
         var directionsDisplay = new google.maps.DirectionsRenderer;
-
         directionsDisplay = new google.maps.DirectionsRenderer({
             suppressMarkers: true
         });
-
-
-
-
-            map = new google.maps.Map(document.getElementById('map-canvas'), {
+        map = new google.maps.Map(document.getElementById('map-canvas'), {
             center: {lat: 54.688486, lng: 25.279861},
             zoom: 14
         });
@@ -65,15 +48,10 @@ if (Meteor.isClient) {
         createMarker();
 
         function createMarker() {
-
-
             var image = {
                 url: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAASAgMAAAAroGbEAAAADFBMVEUAAADAwMD////cMTHYIE0PAAAAAXRSTlMAQObYZgAAAEFJREFUeF5lytEJACEMA9BAB3DPm+kmKAVXEFyoAwgxWPDHBN5HCIBMYXPI1gRILUYu+fkv3eO1PtfOON4dqZaVDa25JKCSbwixAAAAAElFTkSuQmCC',
-                // This marker is 20 pixels wide by 32 pixels high.
                 size: new google.maps.Size(20, 32),
-                // The origin for this image is (0, 0).
                 origin: new google.maps.Point(0, 0),
-                // The anchor for this image is the base of the flagpole at (0, 32).
                 anchor: new google.maps.Point(0, 32)
             };
 
@@ -84,18 +62,14 @@ if (Meteor.isClient) {
                 icon: image
             });
 
-
+            //observing changes
             Markers.find().observe({
-                changed: function(first, second) {
-                    marker.setPosition({ lat: Number(second.lat), lng: Number(second.lng) });
+                changed: function (first, second) {
+                    marker.setPosition({lat: Number(second.lat), lng: Number(second.lng)});
                 }
             });
 
             marker.addListener('click', test);
-        }
-
-        function test() {
-            console.log('clicked');
         }
 
         directionsDisplay.setMap(map);
